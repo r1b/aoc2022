4089ab3f71a9a1c7496204615b4aa36db488691a
r1b <robert.cole.jensen@gmail.com>
2022-12-06 22:08:50 -0500

    day6: part1 + part2

---
+++ day6/part2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// --------------------------------------------------------------------------------
+
+const WINDOW_SIZE = 14
+
+// --------------------------------------------------------------------------------
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	buf := scanner.Text()
+
+	counts := make(map[byte]int)
+	start := 0
+	end := WINDOW_SIZE
+
+	for i := start; i < end; i++ {
+		counts[buf[i]] += 1
+	}
+
+	for ; end < len(buf); start, end = start+1, end+1 {
+		if len(counts) == WINDOW_SIZE {
+			fmt.Println("Found marker", end)
+			return
+		}
+
+		startChar := buf[start]
+		endChar := buf[end]
+
+		counts[startChar] -= 1
+		if counts[startChar] == 0 {
+			delete(counts, startChar)
+		}
+
+		counts[endChar] += 1
+	}
+}
